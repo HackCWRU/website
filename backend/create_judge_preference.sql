39f5de76a9a45e6bbb291ec44d2c3a25a509872c
Ben Young <the12thbenyoung@gmail.com>
2021-04-14 19:22:58 -0400

    sql script to make JudgePreference table

---
+++ backend/create_judge_preference.sql
@@ -0,0 +1,7 @@
+CREATE TABLE JudgePreference (
+    judge_id int NOT NULL,
+    prize_name varchar(255) NOT NULL,
+    PRIMARY KEY (judge_id, prize_name),
+    FOREIGN KEY (judge_id) REFERENCES Judge(judge_id),
+    FOREIGN KEY (prize_name) REFERENCES Prize(prize_name)
+);
